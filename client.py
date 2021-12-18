e36f394067ad2f559ef683250213a9e7c67be0b4
Jorge Kleinerman <jkleinerman@gmail.com>
2021-12-18 17:41:55 -0300

    Docstring and comments in server and client

--- client.py
+++ client.py
@@ -5,18 +5,25 @@ import argparse
 
 import server
 
-
+# Exit code when there is an error
 EXIT_FAILURE = 1
+
+# Dictionary of functions applied to list of data to coalesce data.
 strategy_funcs = {'min': min, 'max': max, 'mean': statistics.mean}
 
 
 class ApiClient(object):
+    """Simulates the API client."""
 
     def __init__(self,
                  server: server.ApiServer,
                  coalesce_strategy: str,
                  *base_api_urls: list[str]):
         """
+        Inits the client "connecting" to a server previously created.
+        Establishes a default coalesce strategy.
+        Receives a variable amount of API URLs that then will be used
+        to coalesce the data according to the coalesce strategy.
         """
         self.api_server = api_server
         try:
@@ -35,37 +42,46 @@ class ApiClient(object):
 
     def get_from_srv(self, member_id: int):
         """
+        Connects to all server APIs that are in base_api_urls list with
+        member_id received as argument, to fill deductible_list,
+        stop_loss_list and oop_max_list lists.
+        Since the server sends the response as string, it is converted
+        to dictionary with json module
         """
 
-        # Clean if there is old data
+        # Cleans the lists if there is old data
         self.deductible_list = []
         self.stop_loss_list = []
         self.oop_max_list = []
 
         for base_api_url in self.base_api_urls:
             api_url = base_api_url + f'?member_id={member_id}'
-            server_resp = json.loads(self.api_server.answer_client(api_url))
+            server_resp = json.loads(self.api_server.respond_client(api_url))
 
             try:
                 self.deductible_list.append(server_resp['deductible'])
                 self.stop_loss_list.append(server_resp['stop_loss'])
                 self.oop_max_list.append(server_resp['oop_max'])
             except KeyError:
-                print('There is missing information in server response')
+                print('There is missing information in server response.')
 
 
     def output_to_user(self) -> str:
         """
+        Returns a string with the format of a JSON with the values
+        deductible, stop_loss and oop_max according to the coalesce strategy.
         """
 
         try:
             deductible = self.coalesce_strategy(self.deductible_list)
-            deductible = round(deductible, 2)
+            deductible = round(deductible, 2) # makes sense in case mean strat.
             stop_loss = self.coalesce_strategy(self.stop_loss_list)
-            stop_loss = round(stop_loss, 2)
+            stop_loss = round(stop_loss, 2) # makes sense in case mean strat.
             oop_max = self.coalesce_strategy(self.oop_max_list)
-            oop_max = round(oop_max, 2)
+            oop_max = round(oop_max, 2) # makes sense in case mean strat.
         except (ValueError, statistics.StatisticsError):
+            # This exception can happen when one of the lists are empty
+            # For example: get_from_srv() was NOT called yet
             print("Client didn't get server information or server"
                   " didn't send all the information."
                  )
@@ -102,12 +118,21 @@ if __name__ == "__main__":
     strategy = args['strategy']
 
 
+    base_api_urls = [
+        'https://api1.com',
+        'https://api2.com',
+        'https://api3.com'
+    ]
+
     api_server = server.ApiServer()
-    api_client = ApiClient(api_server, strategy,
-                           'https://api1.com',
-                           'https://api2.com',
-                           'https://api3.com')
+    api_client = ApiClient(api_server, strategy, *base_api_urls)
 
     api_client.get_from_srv(member_id)
+    result = api_client.output_to_user()
 
-    print(api_client.output_to_user())
+    print('For the following APIs:')
+    for base_api_url in base_api_urls:
+        print(f'- {base_api_url}')
+    print(f'Using member ID: {member_id} and Coalesce Strategy: {strategy}\n'
+          f'The result is: {result}'
+         )
--- server.py
+++ server.py
@@ -2,22 +2,39 @@ import random
 import re
 
 class ApiServer(object):
+    """
+    Simulates the API server.
+    Every time the client connects the server, it will respond
+    with different values of: deductible, stop_loss and oop_max
+    gotten randomly from the intervals received in the constructor
+    """
 
     def __init__(self,
-                 deductible_interval: tuple[int, int]=(500, 2000),
-                 stop_loss_interval: tuple[int, int]=(5000, 15000),
-                 oop_max_interval: tuple[int, int]=(3000, 7000)):
+                 deductible_interval: tuple[int, int]=(1000, 1200),
+                 stop_loss_interval: tuple[int, int]=(10000, 13000),
+                 oop_max_interval: tuple[int, int]=(5000, 6000)):
         """
+        Limits deductible_interval, stop_loss_interval and opp_max_interval
+        to let the function respond_client(), chose values randomly from them.
+        By default it is limiting them with the values given in the problem
+        statement.
         """
         self.deductible_interval = deductible_interval
         self.stop_loss_interval = stop_loss_interval
         self.oop_max_interval = oop_max_interval
 
 
-    def answer_client(self, api_url: str) -> str:
+
+    def respond_client(self, api_url: str) -> str:
         """
+        Responds to client with values of deductible, stop_loss and oop_max
+        chosen randomly from the intervals.
+        To make it more realistic, the random value involves the member_id
+        and the API number received in the URL
         """
 
+        # Gets the API number and member ID from the URL received from
+        # the client
         try:
             api_number = int(re.search('api(\d)', api_url).groups()[0])
             member_id = int(re.search('member_id=(\d)', api_url).groups()[0])
@@ -28,18 +45,24 @@ class ApiServer(object):
         # Combining API number and member_id number to get different random
         # output.
         # As it is a random output it makes no sense, but it is just a way
-        # to use the inputs in the result
+        # to use the inputs to get the result
         step = api_number + member_id
 
         deductible = random.randrange(*self.deductible_interval, step)
         stop_loss = random.randrange(*self.stop_loss_interval, step)
         oop_max = random.randrange(*self.oop_max_interval, step)
 
-        return f'{{"deductible": {deductible}, "stop_loss": {stop_loss}, "oop_max": {oop_max}}}'
+        # Return the result as a string to simulate a kind of
+        # network connection
+        return (f'{{"deductible": {deductible},'
+                f' "stop_loss": {stop_loss},'
+                f' "oop_max": {oop_max}}}'
+               )
 
 
+# A little testing of the server module
 if __name__ == "__main__":
     api_server = ApiServer((600, 3000),
                            (5077, 14045),
                            (3745, 7021))
-    print(api_server.answer_client('https://api2.com?member_id=1'))
+    print(api_server.respond_client('https://api2.com?member_id=1'))
